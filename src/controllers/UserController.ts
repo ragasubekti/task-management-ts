4c01947c0ccc90a8d66fc7e3902f146e8ea09ce9
Raga Subekti <rfilmriot@gmail.com>
2019-07-07 00:59:45 +0700

    Add Register User Fn

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 
-export const Register = (req: Request, res: Response) => {
+import bcrypt from "bcryptjs";
+
+import UserModel from "../models/UserModel";
+import { create } from "domain";
+
+export const Register = async (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -9,4 +14,30 @@ export const Register = (req: Request, res: Response) => {
       errors: errors.mapped()
     });
   }
+
+  const { username, password } = req.body;
+
+  const findSameUsername = await UserModel.findOne({
+    username
+  });
+
+  if (findSameUsername) {
+    return res.status(422).send({
+      message: "Username is already exists"
+    });
+  }
+
+  const salt = await bcrypt.genSaltSync(7);
+  const passwordHash = await bcrypt.hashSync(password, salt);
+
+  const createUser = await new UserModel({
+    username,
+    password: passwordHash,
+    isManager: false
+  }).save();
+
+  return res.status(201).send({
+    message: "Successfully created user",
+    data: createUser
+  });
 };
